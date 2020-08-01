d7944e249d8384ffea95e7a916f71b92e8b795dc
Jimmy <jimmy.moore@gmail.com>
2020-08-01 04:45:10 +0100

    Got past Secio, working on yamux etc now

--- Crawler/src/main/java/net/axod/ipfscrawl/IPFSIOPlugin.java
+++ Crawler/src/main/java/net/axod/ipfscrawl/IPFSIOPlugin.java
@@ -19,6 +19,9 @@ import java.security.spec.*;
 import java.security.interfaces.*;
 import java.util.logging.*;
 
+import javax.crypto.*;
+import javax.crypto.spec.*;
+
 /**
  * This represents a connection to an IPFS node.
  *
@@ -44,12 +47,27 @@ public class IPFSIOPlugin extends IOPlugin {
 	SecioProtos.Exchange local_exchange = null;
 	SecioProtos.Exchange remote_exchange = null;
 
+	boolean we_are_primary = false;
+	
+	Mac incoming_HMAC;
+	Mac outgoing_HMAC;
+	
+	Cipher incoming_cipher;
+	Cipher outgoing_cipher;
+	
+	boolean got_enc_nonce = false;
+	
+	boolean got_enc_multistream = false;
+	boolean using_yamux = false;
+	
 	// My RSA keys
 	KeyPair mykeys = null;
 	
 	// The EC keys
 	KeyPair ec_keys = null;
 
+	byte[] stretched_keys = null;
+	
 	/**
 	 * Given a public key, we can get a Multihash which shows the PeerID in a
 	 * more usable format.
@@ -115,19 +133,74 @@ public class IPFSIOPlugin extends IOPlugin {
 		writeVarInt(data.length);
 		out.put(data);
 	}
+
+	private void writeYamuxMultistreamEnc(String d, int m_stream, short m_flags) {
+		ByteBuffer bbm = ByteBuffer.allocate(8192);
+		byte[] da = d.getBytes();
+		writeVarInt(bbm, da.length);
+		bbm.put(da);
+		bbm.flip();
+		byte[] multi_data = new byte[bbm.remaining()];
+		bbm.get(multi_data);
+
+		ByteBuffer bbo = ByteBuffer.allocate(8192);
+		bbo.put((byte)0);	// ver
+		bbo.put((byte)0);	// data
+		bbo.putShort(m_flags);
+		bbo.putInt(m_stream);	// Stream ID
+		bbo.putInt(multi_data.length);
+		bbo.put(multi_data);
+		writeEnc(bbo);		// Write it out...
+		
+	}
+	
+	private void writeMultistreamEnc(String d) {
+		byte[] da = d.getBytes();
+		ByteBuffer oo = ByteBuffer.allocate(8192);
+		writeVarInt(oo, da.length);
+		oo.put(da);
+		oo.flip();
+		byte[] data = new byte[oo.remaining()];
+		oo.get(data);
+		
+		writeEnc(data);
+	}
+	
+	private void writeEnc(ByteBuffer bb) {
+		bb.flip();
+		byte[] data = new byte[bb.remaining()];
+		bb.get(data);
+		System.out.println("WRITING ENCRYPTED...");
+		showHexData(data);
+		
+		writeEnc(data);
+	}
+	
+	// Write encrypted data...
+	private void writeEnc(byte[] data) {		
+		byte[] enc_data = outgoing_cipher.update(data);
+		byte[] mac_data = outgoing_HMAC.doFinal(enc_data);
+		
+		out.putInt(enc_data.length + mac_data.length);
+		out.put(enc_data);
+		out.put(mac_data);	
+	}
 	
 	/**
 	 * Write a varint out
 	 *
 	 */
 	private void writeVarInt(long v) {
+		writeVarInt(out, v);	
+	}
+	private void writeVarInt(ByteBuffer oo, long v) {
 		while(true) {
 			byte d = (byte)(v & 0x7f);
 			if (v>0x80) {
 				d = (byte) (d | 0x80);	// Signal there's more to come...
-				out.put(d);
+				oo.put(d);
 			} else {
-				out.put(d);
+				oo.put(d);
 				break;
 			}
 			v = v >> 7;
@@ -139,10 +212,13 @@ public class IPFSIOPlugin extends IOPlugin {
 	 *
 	 */
 	private long readVarInt() throws BufferUnderflowException {
+		return readVarInt(in);
+	}
+	private long readVarInt(ByteBuffer bb) throws BufferUnderflowException {
 		long len = 0;
 		int sh = 0;
 		while(true) {
-			int b = ((int)in.get()) & 0xff;
+			int b = ((int)bb.get()) & 0xff;
 			long v = (b & 0x7f);
 			len = len | (v << sh);
 			if ((b & 0x80)==0) break;
@@ -230,24 +306,61 @@ public class IPFSIOPlugin extends IOPlugin {
 							String oexchanges = remote_propose.getExchanges();
 							String ociphers = remote_propose.getCiphers();
 							String ohashes = remote_propose.getHashes();
-							
+
 							// Lets create our own...
 							local_propose = SecioProtos.Propose.newBuilder()
 													  .setRand(ByteString.copyFrom(orand))
 													  .setPubkey(ByteString.copyFrom(opubkey))
-													  .setExchanges(oexchanges)
-													  .setCiphers(ociphers)
-													  .setHashes(ohashes)
+													  .setExchanges("P-256")	//oexchanges)
+													  .setCiphers("AES-256")	//ociphers)
+													  .setHashes("SHA256")		//ohashes)
 													  .build();
 							
 							byte[] odata = local_propose.toByteArray();
-	
+
 							logger.info("Secio local propose\n" + local_propose);							
 							out.putInt(odata.length);
 							out.put(odata);
 
 							got_propose = true;
 						} else if (!got_exchange) {
+							//
+							// Now we have done the Propose, lets decide the order, and then we can decide on exchange, ciphers, hashes etc
+							
+							MessageDigest md = MessageDigest.getInstance("SHA-256");
+							md.update(remote_propose.getPubkey().toByteArray());
+							md.update(local_propose.getRand().toByteArray());
+							byte[] oh1 = md.digest();
+							Multihash h1 = new Multihash(Multihash.Type.sha2_256, oh1);
+							
+							md.reset();
+							md.update(local_propose.getPubkey().toByteArray());
+							md.update(remote_propose.getRand().toByteArray());
+							byte[] oh2 = md.digest();
+							Multihash h2 = new Multihash(Multihash.Type.sha2_256, oh2);
+							
+							String hash1 = h1.toString();
+							String hash2 = h2.toString();
+							
+							logger.info("oh1 = " + SecioHelper.toHexString(oh1) + " " + h1);
+							logger.info("oh2 = " + SecioHelper.toHexString(oh2) + " " + h2);
+							
+							for(int i=0;i<hash1.length();i++) {
+								char c1 = hash1.charAt(i);
+								char c2 = hash2.charAt(i);
+								if (c1==c2) {
+									// Carry on...
+								} else if (c1<c2) {
+									we_are_primary = false;
+									break;
+								} else if (c1>c2) {
+									we_are_primary = true;
+									break;
+								}
+							}
+							
+							logger.info("we_are_primary = " + we_are_primary);
+							
 							// Now we're expecting an Exchange...
 							remote_exchange = SecioProtos.Exchange.parseFrom(data);
 
@@ -304,19 +417,8 @@ public class IPFSIOPlugin extends IOPlugin {
 							
 							got_exchange = true;
 							
-							// TODO: Create shared secret
-														
-							// x,y are from THEIR_PUB
-							// priv is our private key
-							//
-							// ScalarMult returns k*(Bx,By) where k is a number in big-endian form.
-							// ScalarMult(x1, y1 *big.Int, k []byte) (x, y *big.Int)
-							//
-							// Generate shared secret.
-							// secret, _ := curve.ScalarMult(x, y, priv)
 
-							// Private key is an integer (BigInteger).
-							// Public key is a point on the EC (x, y).
+							
 							BigInteger ec_priv = ((ECPrivateKey)ec_keys.getPrivate()).getS();
 
 							System.out.println("Our EC private key\n" + ec_priv);
@@ -326,17 +428,197 @@ public class IPFSIOPlugin extends IOPlugin {
 							System.out.println("Their EC Public key\n" + their_pub);
 
 							// Next we need to perform (their_pub * ec_priv) which will create a new ECPoint
+
+							org.bouncycastle.asn1.x9.X9ECParameters ecp = org.bouncycastle.asn1.sec.SECNamedCurves.getByName("secp256r1");
+							org.bouncycastle.math.ec.ECCurve curve = ecp.getCurve();
+							org.bouncycastle.math.ec.ECPoint p = curve.decodePoint(remote_exchange.getEpubkey().toByteArray());
 							
+							System.out.println("ECPoint p = " + p.getXCoord().toBigInteger() + " " + p.getYCoord().toBigInteger());
+							
+							org.bouncycastle.math.ec.ECPoint q = p.multiply(ec_priv);
+							byte[] ass = q.getEncoded(false);
+
+							System.out.println("Created shared secret");
+							showHexData(ass);
+							
+							byte[] ss = new byte[32];
+							System.arraycopy(ass, 1, ss, 0, 32);		// Try the first 32 bytes for now...
+
+							// For P-256, the shared secret should be 32 bytes...
+/*
+  PubKey256Length* = 65
+  PubKey384Length* = 97
+  PubKey521Length* = 133
+  SecKey256Length* = 32
+  SecKey384Length* = 48
+  SecKey521Length* = 66
+  Sig256Length* = 64
+  Sig384Length* = 96
+  Sig521Length* = 132
+  Secret256Length* = SecKey256Length
+  Secret384Length* = SecKey384Length
+  Secret521Length* = SecKey521Length
+*/
 							
 							// TODO: Key stretching
+
+							stretched_keys = SecioHelper.stretchKeys(ss, "AES-256", "SHA256");
 							
+							System.out.println("== Stretched Keys ==");
+							showHexData(stretched_keys);
+
+							// Now, we should be able to use this to proceed...
+							
+							// stretched_keys has 2 sets of keys in...
+							// 
+							byte[][] keys = SecioHelper.splitStretchedKeys(stretched_keys, "AES-256", "SHA256");
+
+							byte[] liv;
+							byte[] lkey;
+							byte[] lmac;
+							byte[] riv;
+							byte[] rkey;
+							byte[] rmac;
+
+							if (we_are_primary) {
+								liv = keys[0];
+								lkey = keys[1];
+								lmac = keys[2];
+								riv = keys[3];
+								rkey = keys[4];
+								rmac = keys[5];
+							} else {
+								riv = keys[0];
+								rkey = keys[1];
+								rmac = keys[2];
+								liv = keys[3];
+								lkey = keys[4];
+								lmac = keys[5];
+							}
+
+							incoming_HMAC = Mac.getInstance("HmacSHA256");
+							incoming_HMAC.init(new SecretKeySpec(rmac, "HmacSHA256"));
+							outgoing_HMAC = Mac.getInstance("HmacSHA256");
+							outgoing_HMAC.init(new SecretKeySpec(lmac, "HmacSHA256"));
+
+        					incoming_cipher = Cipher.getInstance("AES/CTR/NoPadding");
+        					incoming_cipher.init(Cipher.DECRYPT_MODE, new SecretKeySpec(rkey, "AES"), new IvParameterSpec(riv));
+        					outgoing_cipher = Cipher.getInstance("AES/CTR/NoPadding");
+        					outgoing_cipher.init(Cipher.ENCRYPT_MODE, new SecretKeySpec(lkey, "AES"), new IvParameterSpec(liv));
+        					logger.info("Have initialized HMAC and ciphers");
+        					
 						} else {
-							
-							
 							System.out.println("Secio DATA " + data.length);
 							showHexData(data);
 
+							// First split off the mac. For now assume 32 bytes
+        					byte[] mac = new byte[32];
+        					System.arraycopy(data, data.length - mac.length, mac, 0, mac.length);        					
+        					byte[] datanomac = new byte[data.length - 32];
+        					System.arraycopy(data, 0, datanomac, 0, datanomac.length);
 
+							byte[] sign = incoming_HMAC.doFinal(datanomac);
+							boolean verifies = SecioHelper.toHexString(sign).equals(SecioHelper.toHexString(mac));
+							if (!verifies) {
+								logger.warning("Incorrect MAC!");
+							}
+							
+							System.out.println("\n==== Decryption ====\n");
+        					byte[] plainText = incoming_cipher.update(datanomac);
+
+        					System.out.println("RAW DATA " + (verifies?"Verifies":"DOES NOT VERIFY"));
+							showHexData(plainText);
+							
+							if (!got_enc_nonce) {
+								// check it matches...
+								if (SecioHelper.toHexString(plainText).equals(SecioHelper.toHexString(local_propose.getRand().toByteArray()))) {
+									logger.info("The decrypted nonce matches");	
+								}
+
+								// Now we will send our own...
+								
+								byte[] enc_data = outgoing_cipher.update(remote_propose.getRand().toByteArray());
+								byte[] mac_data = outgoing_HMAC.doFinal(enc_data);
+								
+								out.putInt(enc_data.length + mac_data.length);
+								out.put(enc_data);
+								out.put(mac_data);
+
+								logger.info("Sent our encrypted signed nonce");
+								
+								got_enc_nonce = true;
+							} else {
+								
+								if (!using_yamux) {
+									ByteBuffer inbuff = ByteBuffer.wrap(plainText);
+	
+									int mlen = (int)readVarInt(inbuff);
+									byte[] mdata = new byte[mlen];
+									inbuff.get(mdata);
+									String l = new String(mdata);
+									logger.info("Multistream handshake (" + l.trim() + ")");
+									
+									if (l.equals("/multistream/1.0.0\n")) {
+										// Send it back...
+										writeMultistreamEnc("/multistream/1.0.0\n");						
+										writeMultistreamEnc("/yamux/1.0.0\n");						
+									} else if (l.equals("/yamux/1.0.0\n")) {
+										using_yamux = true;	
+									}
+								} else {
+									System.out.println("Decoding yamux");
+									ByteBuffer inbuff = ByteBuffer.wrap(plainText);
+									byte m_ver = inbuff.get();
+									byte m_type = inbuff.get();
+									short m_flags = inbuff.getShort();
+									int m_stream = inbuff.getInt();
+									int m_length = inbuff.getInt();
+
+									System.out.println("yamux ver=" + m_ver + " type=" + m_type + " flags=" + m_flags + " id=" + m_stream + " len=" + m_length);
+
+									if (m_type==0) { // DATA
+										while(inbuff.remaining()>0) {
+											int mlen = (int)readVarInt(inbuff);
+											byte[] mdata = new byte[mlen];
+											inbuff.get(mdata);
+											String l = new String(mdata);
+											logger.info("Yamux(" + m_stream + ") Multistream handshake (" + l.trim() + ")");
+											
+											if (l.equals("/multistream/1.0.0\n")) {
+												writeYamuxMultistreamEnc("/multistream/1.0.0\n", m_stream, (short)2);
+												writeYamuxMultistreamEnc("/ipfs/id/1.0.0\n", m_stream, (short)0);
+											}
+										}
+									} else if (m_type==1) {	// Window update
+										
+									} else if (m_type==2) { // ping
+										
+									} else if (m_type==3) { // go away
+										
+									}
+									
+								}
+//										  VER 00
+//										  TYPE 01
+//										  FLAGS 00 01
+//										  STREAMID 00 00 00 02
+//									      LENGTH 00 fc 00 00
+
+//  									  VER 00
+//										  TYPE 00
+//										  FLAGS 00 00
+//										  STREAMID 00 00 00 02
+//										  LENGTH 00 00 00 24
+//
+//										  13 / 2f 6d 75 6c 74 69 73 74 72 65 61 6d 2f 31 2e 30 2e 30 0a
+//												"/multistream/1.0.0"
+//  									  0f / 2f 69 70 66 73 2f 69 64 2f 31 2e 30 2e 30 0a
+//												"/ipfs/id/1.0.0"
+
+										
+								
+								
+							}
 						}
 					} catch(BufferUnderflowException bue) {
 						// Don't have the data yet...
@@ -357,6 +639,15 @@ public class IPFSIOPlugin extends IOPlugin {
 		}
 	}
 	
+/*
+RAW DATA Verifies
+  5a 6e ca e5 78 08 5f 82 26 24 bd cb 50 84 c3 1d
+  9f 42 fb 34
+	
+RAW DATA Verifies
+  64 02 37 3e ff ff e6 82 56 09 4c 51 73 f6 16 b2
+  0e be 9a 59
+*/
 	public void closing() {
 		logger.info("Connection closing...");
 	}
--- Crawler/src/main/java/net/axod/protocols/SecioHelper.java
+++ Crawler/src/main/java/net/axod/protocols/SecioHelper.java
@@ -10,6 +10,10 @@ import java.security.*;
 import java.security.spec.*;
 import java.security.interfaces.*;
 
+import javax.crypto.*;
+import javax.crypto.spec.*;
+
+
 public class SecioHelper {
 
 	static SecioProtos.Propose local_propose;
@@ -106,31 +110,8 @@ public class SecioHelper {
         SecureRandom random = SecureRandom.getInstance("SHA1PRNG");
 
         keyGen.initialize(256, random);
-
         KeyPair pair = keyGen.generateKeyPair();
-        PrivateKey priv = pair.getPrivate();
-        PublicKey pub = pair.getPublic();
 
-        System.out.println(priv);
-        System.out.println(pub);
-
-        // Get bytes for pub so we can send...
-        byte[] pubk = pub.getEncoded();
-        String pubkeyenc = Base64.getEncoder().encodeToString(pubk);
-
-        System.out.println("Public Key encoded len " + pubk.length);
-        System.out.println("KEY " + pubkeyenc);
-
-        // Try extracting what we need to go into the packet...
-		byte[] encodedKey = new byte[64];
-		System.arraycopy(pubk, pubk.length - 64, encodedKey, 0, 64);
-
-		// Now we have 64 bytes
-		ECPublicKey tryk = generateP256PublicKeyFromFlatW(encodedKey);
-
-		System.out.println(tryk);
-		System.out.println(Base64.getEncoder().encodeToString(tryk.getEncoded()));
-        
         return pair;
 	}
 	
@@ -143,10 +124,10 @@ public class SecioHelper {
 		byte[] encodedKey = new byte[65];
 		encodedKey[0] = 4;
 		System.arraycopy(pubk, pubk.length - 64, encodedKey, 1, 64);
-		return encodedKey;		
+		return encodedKey;
 	}
-	
-	public static void main(String[] args) {
+
+	public static void testVerify() {
 		try {
 		System.out.println("Going to test verify");	
 
@@ -210,7 +191,249 @@ public class SecioHelper {
 
 		} catch(Exception e) {
 			System.out.println("Exception " + e);	
+		}		
+	}
+
+	public static byte[][] splitStretchedKeys(byte[] data, String cipher, String hasher) {
+		int iv_size = 16;
+		int key_size = 16;		// AES-128 is 16, AES-256 is 32
+		if (cipher.equals("AES-128")) key_size = 16;
+		if (cipher.equals("AES-256")) key_size = 32;
+		int mac_size = 20;
+
+		int offset = 0;
+		byte[] iv1 = new byte[iv_size];
+		System.arraycopy(data, offset, iv1, 0, iv_size);
+		offset+=iv_size;
+		byte[] key1 = new byte[key_size];
+		System.arraycopy(data, offset, key1, 0, key_size);
+		offset+=key_size;
+		byte[] mac1 = new byte[mac_size];
+		System.arraycopy(data, offset, mac1, 0, mac_size);
+		offset+=mac_size;
+		
+		byte[] iv2 = new byte[iv_size];
+		System.arraycopy(data, offset, iv2, 0, iv_size);
+		offset+=iv_size;
+		byte[] key2 = new byte[key_size];
+		System.arraycopy(data, offset, key2, 0, key_size);
+		offset+=key_size;
+		byte[] mac2 = new byte[mac_size];
+		System.arraycopy(data, offset, mac2, 0, mac_size);
+		offset+=mac_size;
+		
+		byte[][] rr = new byte[6][];
+		rr[0] = iv1;
+		rr[1] = key1;
+		rr[2] = mac1;
+		rr[3] = iv2;
+		rr[4] = key2;
+		rr[5] = mac2;
+		
+		return rr;
+	}
+
+	public static byte[] stretchKeys(byte[] ss, String cipher, String hasher) {
+		// For now, let's assume we're using AES-256 and SHA256
+		// Cipher key size 32
+		// IV size 16
+		// Mac key 20
+		
+		int iv_size = 16;
+		int key_size = 16;		// AES-128 is 16, AES-256 is 32
+		if (cipher.equals("AES-128")) key_size = 16;
+		if (cipher.equals("AES-256")) key_size = 32;
+		int mac_size = 20;
+
+		byte[] seed = "key expansion".getBytes();
+		
+		byte[] keydata = new byte[(iv_size + key_size + mac_size) * 2];
+
+		// Need to strip off any leading 0s, because of stupid go.
+		int offset = 0;
+		while(offset<ss.length && ss[offset]==0) offset++;
+		byte[] ss_z = new byte[ss.length - offset];
+		System.arraycopy(ss, offset, ss_z, 0, ss_z.length);
+		
+		try {
+			Mac sha_HMAC = Mac.getInstance("Hmac" + hasher);
+			SecretKeySpec secret_key = new SecretKeySpec(ss_z, "Hmac" + hasher);
+			sha_HMAC.init(secret_key);
+			byte[] idig = sha_HMAC.doFinal(seed);
+				
+			// Now go through...
+			int j = 0;
+			while(j<keydata.length) {
+				sha_HMAC.init(secret_key);
+				sha_HMAC.update(idig);
+				byte[] idigb = sha_HMAC.doFinal(seed);
+
+				// Write it out and continue...
+				int todo = idigb.length;
+				if (j + todo > keydata.length) {
+					todo = keydata.length - j;	
+				}
+				System.arraycopy(idigb, 0, keydata, j, todo);
+				j+=todo;
+				// Now update idig...
+				sha_HMAC.init(secret_key);
+				idig = sha_HMAC.doFinal(idig);
+			}
+		} catch (Exception e) {
+			throw new RuntimeException(e.getMessage(), e);
+		}								
+		return keydata;		
+	}
+	
+	private static void showHexData(byte[] d) {
+		int o = 0;
+		while(true) {
+			String l = "";
+			for(int i=0;i<Math.min(16, d.length - o); i++) {
+				String ch = "00" + Integer.toString(((int)d[o+i]) & 0xff, 16);
+				ch = ch.substring(ch.length() - 2, ch.length());
+				l += " " + ch;
+			}
+			System.out.println(" " + l);
+			o+=16;
+			if (o>=d.length) break;
 		}
+	}
+
+	public static String toHexString(byte[] d) {
+		String o = "";
+		for(int i=0;i<d.length;i++) {
+			String ch = "00" + Integer.toString(((int)d[i]) & 0xff, 16);
+			ch = ch.substring(ch.length() - 2, ch.length());
+			o=o+ch;
+		}
+		return o;
+	}
+	
+	public static byte[] fromHexString(String d) {
+		byte[] o = new byte[d.length() / 2];
+		for(int i=0;i<o.length;i++) {
+			o[i] = (byte)Integer.parseInt(d.substring(i*2, i*2 + 2), 16);
+		}
+		return o;
+	}
+	
+	static String[] secrets = {"4F13360145891C202B74FDCA838A85A37CBAEBF5E0774CC344BD6DABA9C4C86A",
+    					"4ED31DA5EEA36277CA9E1C198F3EBB89AE4A6B18B76E48CAE8AEC23A7D0D4E8F700D6696AB01365278E5C45C2B4B1807",
+    					"01D4FBB5104F9B8DEDA95B447C1401A35F995B6BEFE20DBEFF9F7A13B7DA2831FB5A7EA194C4CE1ECE340B993C4C2C53FE641227DB7428B62BF4083686F6FF8FBE8C",
+    // Edge case (where Go implementation returns 65 bytes of secret)
+    // Nim implementation has leading `00`.
+    					"00691BB84462F460D603B3F5FA0031D8DE195234C65B8890CBB6F84456E9718D4572749FC6040D0602698EEE6CCF6FB83101A26925D1A3AB40FB45BF98EAF06A2693",
+    // AES-256 SHA-512
+    					"1F29EC3E0A07994D2ACCEA23A2F570DA9C7A7E39D5026FE6340C1E551E1ADAAF",
+    					"85C20386C1EA1575DD8D111111DBC8B43CA630BEE4BB9AD91658719FF307C0BE0065935B8B849BE80E0D08A3D39098C3",
+    					"01779213E2993A77F1E1BB3B4DB77B5900B53A3A31CDE95D352C695643879824C8EE6501DC8679F5735869251256830A31357B34FF463B9292C02CD22CD30351C44F",
+    // Edge case (where Go implementation returns 65 bytes of secret)
+    // Nim implementation has leading `00`.
+    					"001CC33294544D898781010258C2FB81F02429D2DD54D0B59B8CD2335F57498FD4E444BEC94DA7BE2BAF3E3796AFA8388F626AEB3178355991985EE5FFC9D1AACAD9"};
+
+    static String[] ivs = {
+    	"F643627AA8B91D40BA644B894C7F148E",
+    	"F1D6521E4EE59248F7CCFA6D6C916A32",
+    	"937D77D24441858AF5040C9A81B3D178",
+    	"C7D6AE667F38A3E0C77F4AC96D82112F",
+    	"735E51C37802A6E72277EE74C829A84D",
+    	"617BAEA342062AE87B7A5D5D9F99371C",
+    	"B535FFA95043C90C5FEEF3654E846445",
+    	"3B2D2219A7EE18AB9164910821955C05",
+
+    	"DACC23805C4ED233A7100A488AB5D68F",
+    	"C5BFA3F8BF0D8436840D1AAAF091BD69",
+    	"54CA4A681AEB8B5793A450100244256F",
+    	"D1EB94C73D4C033EA4130B47669F4485",
+    	"1F1ADF6BBDE1DFC5F922D672D2344F3A",
+    	"A828F6D249F38A917CD297F7BDDE7B70",
+    	"54FC8070579A17F6DAD342174062D60A",
+    	"D33C7696183DA21C5CD40AB677BECE7C"
+    };
+	
+    static String[] cipherKeys = {
+    "8C2964320284FAD935AFEC1AFEC9EEF7",
+    "622A9292256B012F3EBE814C0DB22095",
+    "3171DCDBE794BB6CAADDDD71E1751F2C",
+    "5E1519DFCABA2AF17AA4AA580CC1B76E",
+    "8B7AC311FF7B7EA7B4E55E37688DA2BD",
+    "6BB6E06A3A92D5C300598023330712D4",
+    "D794A6B794C1E3501A24240D348B9A62",
+    "45E2FFAC35B7647AD5045C8581F39BF0",
+
+    "B2F8CDBD11B158DC68120E10A6D04C0B272DC3F698EB56B18094275076307CEB",
+    "E0238BAA6B77646CD708DD00DE1FD17C6BB45F184348F512F4AE64E00CEA37B9",
+    "CE009DE8D1C76C2793540A8B24774E09B0F84590B583F1A0551AC0CF1E911BF9",
+    "ED5F14E36F4F2F80084571B24FD55C870B9C2AD937694B75B90E67D3591DC921",
+    "1607CC9FF2B19E8F0CDA902D5996948E8EA8CFFA03F956038497684088A88B2F",
+    "FC2797D1040FF162A90275EBA3FCC4330C2BDC28D23DA80B89842C2D7A6EFA06",
+    "B83698789ED8E3B44E48EAAEB291B3003AD95FAF344EBA1B9071F4FB46A4E4E9",
+    "5D90C579971B7B7F9ECDE55EBCE8921B807AAD45D61952228758BA80F4490E8F"
+    };
+    
+    static String[] macKeys = {
+    "2C812CB8425299B485CEE0BC97778F540380F14F",
+    "8AB685E8A66256480E794B0ADC09BCF4014883C8",
+    "C68EF3F3102D0CEFC0924FEF17D51FABC23EA54C",
+    "F177BD066555CF25327C32C807D2E44B7DAC3EFF",
+    "57176FF3103FA0F4EB58B9E49133C48B4DE9BDE6",
+    "0C9ECECB80DF43CA2720DD340DD992A80AAB56DB",
+    "028BCC3F5559CF43DA1B0C1A03E263C90D04DD77",
+    "02DAF3FF888999C6121CA50F1D49C10FF55F1ACF",
+
+    "6F015DDA49E0DFABD5532E6CB08709CE43F326EC",
+    "68C703B3867247723D21A8C58BA9109DDBB359EF",
+    "6C91DBB5FE99B94A11D0937D0F4E50F2BCB248F3",
+    "D5A856ECC5820D611111BE8CEAAD781E6E4E549D",
+    "9DCEB01D9657A69D40B1885C392FA850486E32B3",
+    "EE66A1579D732E99A8500F48595BF25289E722DB",
+    "E692EC73B0A2E68625221E1D01BA0E6B24BCB43F",
+    "8613E8F86D2DD1CF3CEDC52AD91423F2F31E0003"
+	};
+
+    
+	public static void main(String[] args) {
+
+		for(int i=0;i<secrets.length;i++) {
+			String cipher = "AES-128";
+			if (i>3) cipher = "AES-256";
+			String hasher = "SHA256";
+			if (i>3) hasher = "SHA512";
+			byte[] keydata = stretchKeys(fromHexString(secrets[i]), cipher, hasher);
+
+			// Recreate the key data from reference...
+			
+			byte[] iv1 = fromHexString(ivs[i*2]);
+			byte[] iv2 = fromHexString(ivs[i*2 + 1]);
+			byte[] cipherKeys1 = fromHexString(cipherKeys[i*2]);
+			byte[] cipherKeys2 = fromHexString(cipherKeys[i*2 + 1]);
+			byte[] macKeys1 = fromHexString(macKeys[i*2]);
+			byte[] macKeys2 = fromHexString(macKeys[i*2 + 1]);
+			
+			byte[] alldata = new byte[iv1.length + cipherKeys1.length + macKeys1.length
+			                        + iv2.length + cipherKeys2.length + macKeys2.length];
+			                        
+			int offset = 0;
+			System.arraycopy(iv1, 0, alldata, offset, iv1.length);
+			offset+=iv1.length;
+			System.arraycopy(cipherKeys1, 0, alldata, offset, cipherKeys1.length);
+			offset+=cipherKeys1.length;
+			System.arraycopy(macKeys1, 0, alldata, offset, macKeys1.length);
+			offset+=macKeys1.length;
+			
+			System.arraycopy(iv2, 0, alldata, offset, iv2.length);
+			offset+=iv2.length;
+			System.arraycopy(cipherKeys2, 0, alldata, offset, cipherKeys2.length);
+			offset+=cipherKeys2.length;
+			System.arraycopy(macKeys2, 0, alldata, offset, macKeys2.length);
+			offset+=macKeys2.length;
+			
+			System.out.println("==== Result ====");
+			System.out.println(toHexString(alldata));
+			System.out.println(toHexString(keydata));
+		}
+		
 	}
 	
 }
