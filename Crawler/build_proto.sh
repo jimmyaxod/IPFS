7cdbf7258fd1c1676cc1189b4049580df0b979b5
Jimmy <jimmy.moore@gmail.com>
2020-08-13 00:11:31 +0100

    Added bitswap proto

--- Crawler/build_proto.sh
+++ Crawler/build_proto.sh
@@ -5,3 +5,5 @@ protoc -Isrc/main/java src/main/java/net/axod/pb/PeerIDKeys.proto --java_out=src
 protoc -Isrc/main/java src/main/java/net/axod/pb/IPFS.proto --java_out=src/main/java
 
 protoc -Isrc/main/java src/main/java/net/axod/pb/dht.proto --java_out=src/main/java
+
+protoc -Isrc/main/java src/main/java/net/axod/pb/bitswap.proto --java_out=src/main/java
